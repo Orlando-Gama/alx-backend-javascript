8ee8b54d7a75579695b375214ce13e20f149e2cd
Orlando-Gama <orlando03gama@gmail.com>
2024-04-30 14:35:30 -0700

    all tasks

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,15 +1,3 @@
-/**
- * Logs message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
-// Arrow function
-const displayMessage = (message) => console.log(message);
+const displayMessage = (d) => console.log(d);
 
-/**
- * Exports displayMessage function.
- * @Module displayMessage
- * @Function
- */
-// Default module export.
-module.exports = displayMessage;i
+module.exports = displayMessage;
